2bde5508e4987ff7bb350b94449c9a50d272aa44
gcallah <gcallah@mac.com>
2023-11-09 11:57:33 -0500

    Adding db_connect and its tests to the repo.

---
+++ data/db_connect.py
@@ -0,0 +1,84 @@
+import os
+
+import pymongo as pm
+
+LOCAL = "0"
+CLOUD = "1"
+
+GAME_DB = 'gamesDB'
+
+client = None
+
+MONGO_ID = '_id'
+
+
+def connect_db():
+    """
+    This provides a uniform way to connect to the DB across all uses.
+    Returns a mongo client object... maybe we shouldn't?
+    Also set global client variable.
+    We should probably either return a client OR set a
+    client global.
+    """
+    global client
+    if client is None:  # not connected yet!
+        print("Setting client because it is None.")
+        if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
+            password = os.environ.get("GAME_MONGO_PW")
+            if not password:
+                raise ValueError('You must set your password '
+                                 + 'to use Mongo in the cloud.')
+            print("Connecting to Mongo in the cloud.")
+            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
+                                    + '@cluster0.eqxbbqd.mongodb.net/'
+                                    + '?retryWrites=true&w=majority')
+            # PA recommends these settings:
+            # + 'connectTimeoutMS=30000&'
+            # + 'socketTimeoutMS=None
+            # + '&connect=false'
+            # + 'maxPoolsize=1')
+            # but they don't seem necessary
+        else:
+            print("Connecting to Mongo locally.")
+            client = pm.MongoClient()
+
+
+def insert_one(collection, doc, db=GAME_DB):
+    """
+    Insert a single doc into collection.
+    """
+    print(f'{db=}')
+    return client[db][collection].insert_one(doc)
+
+
+def fetch_one(collection, filt, db=GAME_DB):
+    """
+    Find with a filter and return on the first doc found.
+    """
+    for doc in client[db][collection].find(filt):
+        if MONGO_ID in doc:
+            # Convert mongo ID to a string so it works as JSON
+            doc[MONGO_ID] = str(doc[MONGO_ID])
+        return doc
+
+
+def del_one(collection, filt, db=GAME_DB):
+    """
+    Find with a filter and return on the first doc found.
+    """
+    client[db][collection].delete_one(filt)
+
+
+def fetch_all(collection, db=GAME_DB):
+    ret = []
+    for doc in client[db][collection].find():
+        ret.append(doc)
+    return ret
+
+
+def fetch_all_as_dict(key, collection, db=GAME_DB):
+    ret = {}
+    for doc in client[db][collection].find():
+        del doc[MONGO_ID]
+        ret[doc[key]] = doc
+    return ret
---
+++ data/tests/test_db_connect.py
@@ -0,0 +1,27 @@
+import pytest
+
+import data.db_connect as dbc
+
+TEST_DB = dbc.GAME_DB
+TEST_COLLECT = 'test_collect'
+# can be used for field and value:
+TEST_NAME = 'test'
+
+
+@pytest.fixture(scope='function')
+def temp_rec():
+    dbc.connect_db()
+    dbc.client[TEST_DB][TEST_COLLECT].insert_one({TEST_NAME: TEST_NAME})
+    # yield to our test function
+    yield
+    dbc.client[TEST_DB][TEST_COLLECT].delete_one({TEST_NAME: TEST_NAME})
+
+
+def test_fetch_one(temp_rec):
+    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: TEST_NAME})
+    assert ret is not None
+
+
+def test_fetch_one_not_there(temp_rec):
+    ret = dbc.fetch_one(TEST_COLLECT, {TEST_NAME: 'not a field value in db!'})
+    assert ret is None
