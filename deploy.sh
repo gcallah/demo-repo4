1449b67703d7f4b1b3cae00da560e2d7b13ca478
gcallah <gcallah@mac.com>
2024-01-30 11:25:52 -0500

    Changed PA_PWD to DEMO_PA_PWD in deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,16 @@ echo "Project dir = $PROJ_DIR"
 echo "PA domain = $PA_DOMAIN"
 echo "Virtual env = $VENV"
 
-if [ -z "$PA_PWD" ]
+if [ -z "$DEMO_PA_PWD" ]
 then
-    echo "The PythonAnywhere password var (PA_PWD) must be set in the env."
+    echo "The PythonAnywhere password var (DEMO_PA_PWD) must be set in the env."
     exit 1
 fi
 
 echo "PA user = $PA_USER"
-echo "PA password = $PA_PWD"
+echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
+sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
     cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
 EOF
