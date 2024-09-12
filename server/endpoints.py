cd15eee2db137ad4362b6fb7e295294137189e83
gcallah <gcallah@mac.com>
2024-09-12 00:12:32 -0400

    Getting repo ready for Fall 2024.

--- server/endpoints.py
+++ server/endpoints.py
@@ -2,40 +2,22 @@
 This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
-from http import HTTPStatus
+# from http import HTTPStatus
 
-from flask import Flask, request
-from flask_restx import Resource, Api, fields
+from flask import Flask  # , request
+from flask_restx import Resource, Api  # Namespace, fields
 from flask_cors import CORS
 
-import werkzeug.exceptions as wz
-
-import data.games as gm
-import data.users as users
+# import werkzeug.exceptions as wz
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-DELETE = 'delete'
-DEFAULT = 'Default'
-MENU = 'menu'
-MAIN_MENU_EP = '/MainMenu'
-MAIN_MENU_NM = "Welcome to Text Game!"
+ENDPOINT_EP = '/endpoints'
+ENDPOINT_RESP = 'Available endpoints'
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
-GAMES_EP = '/games'
-DEL_GAME_EP = f'{GAMES_EP}/{DELETE}'
-GAME_MENU_EP = '/game_menu'
-GAME_MENU_NM = 'Game Menu'
-GAME_ID = 'Game ID'
-USERS_EP = '/users'
-USER_MENU_EP = '/user_menu'
-USER_MENU_NM = 'User Menu'
-TYPE = 'Type'
-DATA = 'Data'
-TITLE = 'Title'
-RETURN = 'Return'
 
 
 @api.route(HELLO_EP)
@@ -64,148 +46,3 @@ class Endpoints(Resource):
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
-
-
-@api.route(f'{MAIN_MENU_EP}')
-class MainMenu(Resource):
-    """
-    This will deliver our main menu.
-    """
-    def get(self):
-        """
-        Gets the main game menu.
-        """
-        return {TITLE: MAIN_MENU_NM,
-                DEFAULT: 2,
-                'Choices': {
-                    '1': {'url': '/', 'method': 'get',
-                          'text': 'List Available Characters'},
-                    '2': {'url': '/',
-                          'method': 'get', 'text': 'List Active Games'},
-                    '3': {'url': f'{USERS_EP}',
-                          'method': 'get', 'text': 'List Users'},
-                    '4': {'url': '/',
-                          'method': 'get', 'text': 'Illustrating a Point!'},
-                    'X': {'text': 'Exit'},
-                }}
-
-
-@api.route(f'{USER_MENU_EP}')
-class UserMenu(Resource):
-    """
-    This will deliver our user menu.
-    """
-    def get(self):
-        """
-        Gets the user menu.
-        """
-        return {
-                   TITLE: USER_MENU_NM,
-                   DEFAULT: '0',
-                   'Choices': {
-                       '1': {
-                            'url': '/',
-                            'method': 'get',
-                            'text': 'Get User Details',
-                       },
-                       '0': {
-                            'text': 'Return',
-                       },
-                   },
-               }
-
-
-@api.route(f'{USERS_EP}')
-class Users(Resource):
-    """
-    This class supports fetching a list of all users.
-    """
-    def get(self):
-        """
-        This method returns all users.
-        """
-        return {
-            TYPE: DATA,
-            TITLE: 'Current Games',
-            DATA: users.get_users(),
-            MENU: USER_MENU_EP,
-            RETURN: MAIN_MENU_EP,
-        }
-
-
-@api.route(f'{DEL_GAME_EP}/<name>')
-class DelGame(Resource):
-    """
-    Deletes a game by name.
-    """
-    @api.response(HTTPStatus.OK, 'Success')
-    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
-    def delete(self, name):
-        """
-        Deletes a game by name.
-        """
-        try:
-            gm.del_game(name)
-            return {name: 'Deleted'}
-        except ValueError as e:
-            raise wz.NotFound(f'{str(e)}')
-
-
-game_fields = api.model('NewGame', {
-    gm.NAME: fields.String,
-    gm.NUM_PLAYERS: fields.Integer,
-})
-
-
-@api.route(f'{GAMES_EP}')
-class Games(Resource):
-    """
-    This class supports various operations on games, such as
-    listing them, and adding a game.
-    """
-    def get(self):
-        """
-        This method returns all games.
-        """
-        return {
-            TYPE: DATA,
-            TITLE: 'Current Games',
-            DATA: gm.get_games(),
-            MENU: GAME_MENU_EP,
-            RETURN: MAIN_MENU_EP,
-        }
-
-    @api.expect(game_fields)
-    @api.response(HTTPStatus.OK, 'Success')
-    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'Not Acceptable')
-    def post(self):
-        """
-        Add a game.
-        """
-        name = request.json[gm.NAME]
-        num_players = request.json[gm.NUM_PLAYERS]
-        try:
-            new_id = gm.add_game(name, num_players)
-            if new_id is None:
-                raise wz.ServiceUnavailable('We have a technical problem.')
-            return {GAME_ID: new_id}
-        except ValueError as e:
-            raise wz.NotAcceptable(f'{str(e)}')
-
-
-@api.route(f'{GAMES_EP}/<name>/<num_players>')
-class NumPlayers(Resource):
-    """
-    Updates the number of players in a game.
-    """
-    @api.response(HTTPStatus.OK, 'Success')
-    @api.response(HTTPStatus.NOT_ACCEPTABLE, 'Not Acceptable')
-    def put(self, name, num_players):
-        """
-        Update the number of players in a game.
-        """
-        try:
-            gm.update_num_players(name, num_players)
-            return {name: 'Updated'}
-        except ValueError as e:
-            raise wz.NotFound(f'{str(e)}')
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -11,8 +11,6 @@ from unittest.mock import patch
 
 import pytest
 
-import data.games as gm
-
 import server.endpoints as ep
 
 TEST_CLIENT = ep.app.test_client()
@@ -22,98 +20,3 @@ def test_hello():
     resp = TEST_CLIENT.get(ep.HELLO_EP)
     resp_json = resp.get_json()
     assert ep.HELLO_RESP in resp_json
-
-
-def test_list_users():
-    resp = TEST_CLIENT.get(ep.USERS_EP)
-    assert resp.status_code == OK
-    resp_json = resp.get_json()
-    assert isinstance(resp_json, dict)
-    assert ep.TITLE in resp_json
-    assert ep.TYPE in resp_json
-    assert ep.DATA in resp_json
-
-
-def test_games_get():
-    resp = TEST_CLIENT.get(ep.GAMES_EP)
-    assert resp.status_code == OK
-    resp_json = resp.get_json()
-    assert isinstance(resp_json, dict)
-
-
-@patch('data.games.update_num_players', autospec=True)
-def test_games_update_num_players(mock_update):
-    """
-    Testing we do the right thing with a call to update_num_players that succeeds.
-    """
-    resp = TEST_CLIENT.put(f'{ep.GAMES_EP}/AnyName/100')
-    assert resp.status_code == OK
-
-
-@patch('data.games.del_game', autospec=True)
-def test_games_del(mock_del):
-    """
-    Testing we do the right thing with a call to del_game that succeeds.
-    """
-    resp = TEST_CLIENT.delete(f'{ep.DEL_GAME_EP}/AnyName')
-    assert resp.status_code == OK
-
-
-@patch('data.games.update_num_players', side_effect=ValueError(), autospec=True)
-def test_games_bad_update_num_players(mock_update):
-    """
-    Testing we do the right thing with a call to update_num_players that fails.
-    """
-    resp = TEST_CLIENT.put(f'{ep.GAMES_EP}/AnyName/100')
-    assert resp.status_code == NOT_FOUND
-
-
-@patch('data.games.del_game', autospec=True)
-def test_games_del(mock_del):
-    """
-    Testing we do the right thing with a call to del_game that succeeds.
-    """
-    resp = TEST_CLIENT.delete(f'{ep.DEL_GAME_EP}/AnyName')
-    assert resp.status_code == OK
-
-
-@patch('data.games.del_game', side_effect=ValueError(), autospec=True)
-def test_games_bad_del(mock_del):
-    """
-    Testing we do the right thing with a value error from del_game.
-    """
-    resp = TEST_CLIENT.delete(f'{ep.DEL_GAME_EP}/AnyName')
-    assert resp.status_code == NOT_FOUND
-
-
-@patch('data.games.add_game', return_value=gm.MOCK_ID, autospec=True)
-def test_games_add(mock_add):
-    """
-    Testing we do the right thing with a good return from add_game.
-    """
-    resp = TEST_CLIENT.post(ep.GAMES_EP, json=gm.get_test_game())
-    assert resp.status_code == OK
-
-
-@patch('data.games.add_game', side_effect=ValueError(), autospec=True)
-def test_games_bad_add(mock_add):
-    """
-    Testing we do the right thing with a value error from add_game.
-    """
-    resp = TEST_CLIENT.post(ep.GAMES_EP, json=gm.get_test_game())
-    assert resp.status_code == NOT_ACCEPTABLE
-
-
-@patch('data.games.add_game', return_value=None)
-def test_games_add_db_failure(mock_add):
-    """
-    Testing we do the right thing with a null ID return from add_game.
-    """
-    resp = TEST_CLIENT.post(ep.GAMES_EP, json=gm.get_test_game())
-    assert resp.status_code == SERVICE_UNAVAILABLE
-
-
-@pytest.mark.skip('This test is failing, but it is just an example of using '
-                   + 'skip')
-def test_that_doesnt_work():
-    assert False
