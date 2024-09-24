24b577e9c2009627c41d21a475653dc02e479d77
gcallah <gcallah@mac.com>
2024-09-24 12:21:23 -0400

    Added title endpoint and a test for it.

--- server/endpoints.py
+++ server/endpoints.py
@@ -18,6 +18,13 @@ ENDPOINT_EP = '/endpoints'
 ENDPOINT_RESP = 'Available endpoints'
 HELLO_EP = '/hello'
 HELLO_RESP = 'hello'
+TITLE_EP = '/title'
+TITLE_RESP = 'Title'
+TITLE = 'The Journal of API Technology'
+EDITOR_RESP = 'Editor'
+EDITOR = 'ejc369@nyu.edu'
+DATE_RESP = 'Date'
+DATE = '2024-09-24'
 
 
 @api.route(HELLO_EP)
@@ -29,12 +36,11 @@ class HelloWorld(Resource):
     def get(self):
         """
         A trivial endpoint to see if the server is running.
-        It just answers with "hello world."
         """
         return {HELLO_RESP: 'world'}
 
 
-@api.route('/endpoints')
+@api.route(ENDPOINT_EP)
 class Endpoints(Resource):
     """
     This class will serve as live, fetchable documentation of what endpoints
@@ -42,7 +48,24 @@ class Endpoints(Resource):
     """
     def get(self):
         """
-        The `get()` method will return a list of available endpoints.
+        The `get()` method will return a sorted list of available endpoints.
         """
         endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
+
+
+@api.route(TITLE_EP)
+class JournalTitle(Resource):
+    """
+    This class handles creating, reading, updating
+    and deleting the journal title.
+    """
+    def get(self):
+        """
+        Retrieve the journal title.
+        """
+        return {
+            TITLE_RESP: TITLE,
+            EDITOR_RESP: EDITOR,
+            DATE_RESP: DATE,
+        }
--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -20,3 +20,13 @@ def test_hello():
     resp = TEST_CLIENT.get(ep.HELLO_EP)
     resp_json = resp.get_json()
     assert ep.HELLO_RESP in resp_json
+
+
+def test_title():
+    resp = TEST_CLIENT.get(ep.TITLE_EP)
+    print(f'{ep.TITLE_EP=}')
+    resp_json = resp.get_json()
+    print(f'{resp_json=}')
+    assert ep.TITLE_RESP in resp_json
+    assert isinstance(resp_json[ep.TITLE_RESP], str)
+    assert len(resp_json[ep.TITLE_RESP]) > 0
