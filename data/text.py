cca95546db7b1d5680b5359de2d4a9ac48e8d5f3
gcallah <gcallah@mac.com>
2024-10-14 10:38:50 -0400

    Started text module.

---
+++ data/text.py
@@ -0,0 +1,42 @@
+"""
+This module interfaces to our user data.
+"""
+
+# fields
+KEY = 'key'
+TITLE = 'title'
+TEXT = 'text'
+EMAIL = 'email'
+
+TEST_KEY = 'HomePage'
+DEL_KEY = 'DeletePage'
+
+text_dict = {
+    TEST_KEY: {
+        TITLE: 'Home Page',
+        TEXT: 'This is a journal about building API servers.',
+    },
+    DEL_KEY: {
+        TITLE: 'Home Page',
+        TEXT: 'This is a journal about building API servers.',
+    },
+}
+
+
+def read():
+    """
+    Our contract:
+        - No arguments.
+        - Returns a dictionary of users keyed on user email.
+        - Each user email must be the key for another dictionary.
+    """
+    text = text_dict
+    return text
+
+
+def main():
+    print(read())
+
+
+if __name__ == '__main__':
+    main()
