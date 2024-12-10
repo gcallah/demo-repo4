73fe43dd8020a03907cd158d5f551709b70bd6b3
Gene Callahan <gcallah@mac.com>
2024-12-10 12:01:17 -0500

    Added real assign ref and delete ref functions.

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -6,6 +6,7 @@ COPY_EDIT = 'CED'
 IN_REF_REV = 'REV'
 REJECTED = 'REJ'
 SUBMITTED = 'SUB'
+WITHDRAWN = 'WIT'
 TEST_STATE = SUBMITTED
 
 VALID_STATES = [
@@ -14,6 +15,7 @@ VALID_STATES = [
     IN_REF_REV,
     REJECTED,
     SUBMITTED,
+    WITHDRAWN,
 ]
 
 
@@ -35,16 +37,20 @@ def is_valid_state(state: str) -> bool:
 # actions:
 ACCEPT = 'ACC'
 ASSIGN_REF = 'ARF'
+DELETE_REF = 'DRF'
 DONE = 'DON'
 REJECT = 'REJ'
+WITHDRAW = 'WIT'
 # for testing:
 TEST_ACTION = ACCEPT
 
 VALID_ACTIONS = [
     ACCEPT,
     ASSIGN_REF,
+    DELETE_REF,
     DONE,
     REJECT,
+    WITHDRAW,
 ]
 
 
@@ -56,33 +62,62 @@ def is_valid_action(action: str) -> bool:
     return action in VALID_ACTIONS
 
 
-def sub_assign_ref(manu: dict) -> str:
+def assign_ref(manu: dict, ref: str, extra=None) -> str:
+    print(extra)
+    manu[flds.REFEREES].append(ref)
     return IN_REF_REV
+
+
+def delete_ref(manu: dict, ref: str) -> str:
+    if len(manu[flds.REFEREES]) > 0:
+        manu[flds.REFEREES].remove(ref)
+    if len(manu[flds.REFEREES]) > 0:
+        return IN_REF_REV
+    else:
+        return SUBMITTED
 
 
 FUNC = 'f'
 
+COMMON_ACTIONS = {
+    WITHDRAW: {
+        FUNC: lambda **kwargs: WITHDRAWN,
+    },
+}
+
 STATE_TABLE = {
     SUBMITTED: {
         ASSIGN_REF: {
-            # These next lines are alternatives that work the same.
-            # FUNC: sub_assign_ref,
-            FUNC: lambda m: IN_REF_REV,
+            FUNC: assign_ref,
         },
         REJECT: {
-            FUNC: lambda m: REJECTED,
+            FUNC: lambda **kwargs: REJECTED,
         },
+        **COMMON_ACTIONS,
     },
     IN_REF_REV: {
+        ASSIGN_REF: {
+            FUNC: assign_ref,
+        },
+        DELETE_REF: {
+            FUNC: delete_ref,
+        },
+        **COMMON_ACTIONS,
     },
     COPY_EDIT: {
         DONE: {
-            FUNC: lambda m: AUTHOR_REV,
+            FUNC: lambda **kwargs: AUTHOR_REV,
         },
+        **COMMON_ACTIONS,
     },
     AUTHOR_REV: {
+        **COMMON_ACTIONS,
     },
     REJECTED: {
+        **COMMON_ACTIONS,
+    },
+    WITHDRAWN: {
+        **COMMON_ACTIONS,
     },
 }
 
@@ -93,17 +128,25 @@ def get_valid_actions_by_state(state: str):
     return valid_actions
 
 
-def handle_action(curr_state, action, manuscript) -> str:
+def handle_action(curr_state, action, **kwargs) -> str:
     if curr_state not in STATE_TABLE:
         raise ValueError(f'Bad state: {curr_state}')
     if action not in STATE_TABLE[curr_state]:
         raise ValueError(f'{action} not available in {curr_state}')
-    return STATE_TABLE[curr_state][action][FUNC](manuscript)
+    return STATE_TABLE[curr_state][action][FUNC](**kwargs)
 
 
 def main():
-    print(handle_action(SUBMITTED, ASSIGN_REF, SAMPLE_MANU))
-    print(handle_action(SUBMITTED, REJECT, SAMPLE_MANU))
+    print(handle_action(SUBMITTED, ASSIGN_REF,
+                        manu=SAMPLE_MANU, ref='Jack'))
+    print(handle_action(IN_REF_REV, ASSIGN_REF, manu=SAMPLE_MANU,
+                        ref='Jill', extra='Extra!'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jill'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jack'))
+    print(handle_action(SUBMITTED, WITHDRAW, manu=SAMPLE_MANU))
+    print(handle_action(SUBMITTED, REJECT, manu=SAMPLE_MANU))
 
 
 if __name__ == '__main__':
--- data/manuscripts/tests/test_query.py
+++ data/manuscripts/tests/test_query.py
@@ -42,14 +42,14 @@ def test_handle_action_bad_state():
     with pytest.raises(ValueError):
         mqry.handle_action(gen_random_not_valid_str(),
                            mqry.TEST_ACTION,
-                           mqry.SAMPLE_MANU)
+                           manu=mqry.SAMPLE_MANU)
 
 
 def test_handle_action_bad_action():
     with pytest.raises(ValueError):
         mqry.handle_action(mqry.TEST_STATE,
                            gen_random_not_valid_str(),
-                           mqry.SAMPLE_MANU)
+                           manu=mqry.SAMPLE_MANU)
 
 
 def test_handle_action_valid_return():
@@ -57,6 +57,7 @@ def test_handle_action_valid_return():
         for action in mqry.get_valid_actions_by_state(state):
             print(f'{action=}')
             new_state = mqry.handle_action(state, action,
-                                           mqry.SAMPLE_MANU)
+                                           manu=mqry.SAMPLE_MANU,
+                                           ref='Some ref')
             print(f'{new_state=}')
             assert mqry.is_valid_state(new_state)
