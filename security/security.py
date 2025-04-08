3c6727bb6f884be85517b60fe54d5550d5ab07a0
gcallah <gcallah@mac.com>
2025-04-08 12:22:40 -0400

    Finishing security module.

--- security/security.py
+++ security/security.py
@@ -56,28 +56,61 @@ DELETE = 'delete'
 USER_LIST = 'user_list'
 CHECKS = 'checks'
 LOGIN = 'login'
+LOGIN_KEY = 'login_key'
+
 
 # Features:
 PEOPLE = 'people'
+BAD_FEATURE = 'baaaad feature'
+
+PEOPLE_MISSING_ACTION = READ
+GOOD_USER_ID = 'ejc369@nyu.edu'
 
 security_recs = None
 # These will come from the DB soon:
-temp_recs = {
+TEST_RECS = {
     PEOPLE: {
         CREATE: {
-            USER_LIST: ['ejc369@nyu.edu'],
+            USER_LIST: [GOOD_USER_ID],
             CHECKS: {
                 LOGIN: True,
+            },
+        },
+    },
+    BAD_FEATURE: {
+        CREATE: {
+            USER_LIST: [GOOD_USER_ID],
+            CHECKS: {
+                'Bad check': True,
             },
         },
     },
 }
 
 
+def is_valid_key(user_id: str, login_key: str):
+    """
+    This is just a mock of the real is_valid_key() we'll write later.
+    """
+    return True
+
+
+def check_login(user_id: str, **kwargs):
+    if LOGIN_KEY not in kwargs:
+        return False
+    return is_valid_key(user_id, kwargs[LOGIN_KEY])
+
+
+CHECK_FUNCS = {
+    LOGIN: check_login,
+    # IP_ADDRESS: check_ip,
+}
+
+
 def read() -> dict:
     global security_recs
     # dbc.read()
-    security_recs = temp_recs
+    security_recs = TEST_RECS
     return security_recs
 
 
@@ -100,3 +133,24 @@ def read_feature(feature_name: str) -> dict:
         return security_recs[feature_name]
     else:
         return None
+
+
+@needs_recs
+def is_permitted(feature_name: str, action: str,
+                 user_id: str, **kwargs) -> bool:
+    prot = read_feature(feature_name)
+    if prot is None:
+        return True
+    if action not in prot:
+        return True
+    if USER_LIST in prot[action]:
+        if user_id not in prot[action][USER_LIST]:
+            return False
+    if CHECKS not in prot[action]:
+        return True
+    for check in prot[action][CHECKS]:
+        if check not in CHECK_FUNCS:
+            raise ValueError(f'Bad check passed to is_permitted: {check}')
+        if not CHECK_FUNCS[check](user_id, **kwargs):
+            return False
+    return True
--- security/tests/test_security.py
+++ security/tests/test_security.py
@@ -1,4 +1,15 @@
+import pytest
+
 import security.security as sec
+
+
+def test_check_login_good():
+    assert sec.check_login(sec.GOOD_USER_ID,
+                           login_key='any key will do for now')
+
+
+def test_check_login_bad():
+    assert not sec.check_login(sec.GOOD_USER_ID)
 
 
 def test_read():
@@ -7,3 +18,30 @@ def test_read():
     for feature in recs:
         assert isinstance(feature, str)
         assert len(feature) > 0
+
+
+def test_read_feature():
+    feature = sec.read_feature(sec.PEOPLE)
+    assert isinstance(feature, dict)
+
+
+def test_is_permitted_no_such_feature():
+    assert sec.is_permitted('Non-existent feature', sec.CREATE, 'any user')
+
+
+def test_is_permitted_action_missing():
+    assert sec.is_permitted(sec.PEOPLE, sec.PEOPLE_MISSING_ACTION, 'any user')
+
+
+def test_is_permitted_bad_user():
+    assert not sec.is_permitted(sec.PEOPLE, sec.CREATE, 'non-existent user')
+
+
+def test_is_permitted_bad_check():
+    with pytest.raises(ValueError):
+        sec.is_permitted(sec.BAD_FEATURE, sec.CREATE, sec.GOOD_USER_ID)
+
+
+def test_is_permitted_all_good():
+    assert sec.is_permitted(sec.PEOPLE, sec.CREATE, sec.GOOD_USER_ID,
+                            login_key='any key for now')
